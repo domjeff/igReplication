9231ba1353064058428c2e06cc88f7f5b5749509
CADIRP43s@example.com <CADIRP43s@example.com>
2021-04-19 04:37:32 +0700

    change green tiles to network image

--- lib/components/photoGallery/photoGallery.dart
+++ lib/components/photoGallery/photoGallery.dart
@@ -18,13 +18,22 @@ class _PhotoGalleryState extends State<PhotoGallery> {
       crossAxisCount: 4,
       itemCount: widget.itemCount,
       itemBuilder: (BuildContext context, int index) => new Container(
-          color: Colors.green,
-          child: new Center(
-            child: new CircleAvatar(
-              backgroundColor: Colors.white,
-              child: new Text('$index'),
+        color: Colors.black,
+        child: Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              fit: BoxFit.contain,
+              image: new NetworkImage("https://i.imgur.com/BoN9kdC.png"),
             ),
-          )),
+          ),
+        ),
+//        new Center(
+//          child: new CircleAvatar(
+//            backgroundColor: Colors.white,
+//            child: new Text('$index'),
+//          ),
+//        ),
+      ),
       staggeredTileBuilder: (int index) =>
           new StaggeredTile.count(2, widget.itemCount / 8),
       mainAxisSpacing: 4.0,
